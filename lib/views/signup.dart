067956d4fca5446d25556f92939411aec402bb65
Abhinav Gupta <abhi30092000.ag@gmail.com>
2021-07-02 00:56:17 +0530

    added validation to sign up screen

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,16 +1,25 @@
 import 'package:chat_app/widgets/widget.dart';
 import 'package:flutter/material.dart';
+
 class SignUp extends StatefulWidget {
-
-
   @override
   _SignUpState createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  TextEditingController userNameTextEditingController = new TextEditingController();
-  TextEditingController emailTextEditingController = new TextEditingController();
-  TextEditingController passwordTextEditingController = new TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  TextEditingController userNameTextEditingController =
+      new TextEditingController();
+  TextEditingController emailTextEditingController =
+      new TextEditingController();
+  TextEditingController passwordTextEditingController =
+      new TextEditingController();
+
+  signMeUp(){
+    if(formKey.currentState.validate()){
+
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +29,37 @@ class _SignUpState extends State<SignUp> {
           padding: EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             children: [
-              SizedBox(height: 15,),
-              TextField(
-                controller: userNameTextEditingController,
-                decoration: textFieldInputDecoration('Username'),
+              SizedBox(
+                height: 15,
               ),
-              TextField(
-                controller: emailTextEditingController,
-                decoration: textFieldInputDecoration('Email'),
-              ),
-              TextField(
-                controller: passwordTextEditingController,
-                decoration: textFieldInputDecoration('Password'),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (val){
+                        return val.isEmpty || val.length < 4 ? "Username is not valid" : null;
+                      },
+                      controller: userNameTextEditingController,
+                      decoration: textFieldInputDecoration('Username'),
+                    ),
+                    TextFormField(
+                      validator: (val){
+                        return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ?
+                          null : "Enter correct email";
+                      },
+                      controller: emailTextEditingController,
+                      decoration: textFieldInputDecoration('Email'),
+                    ),
+                    TextFormField(
+                      validator: (val){
+                        return  val.length > 6 ?  null : "Password should be of atleast 6 characters";
+                      },
+                      controller: passwordTextEditingController,
+                      decoration: textFieldInputDecoration('Password'),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 8,
@@ -49,22 +77,27 @@ class _SignUpState extends State<SignUp> {
               SizedBox(
                 height: 10,
               ),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 18),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                    colors: [
-                      const Color(0xff007EF4),
-                      const Color(0xff2A75BC),
-                    ],
+              GestureDetector(
+                onTap: () {
+                  signMeUp();
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 18),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC),
+                      ],
+                    ),
                   ),
-                ),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
                 ),
               ),
               SizedBox(
