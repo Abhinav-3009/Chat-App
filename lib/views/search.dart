6f0993fb1a63448f3536736cb1672eda0ce7961b
Abhinav Gupta <abhi30092000.ag@gmail.com>
2021-07-03 00:14:33 +0530

    completed search by username functionality

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/services/database.dart';
 import 'package:chat_app/widgets/widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -9,7 +10,37 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   DataBaseMethods dataBaseMethods = new DataBaseMethods();
-  TextEditingController searchTextEditingController= new TextEditingController();
+  TextEditingController searchTextEditingController =
+      new TextEditingController();
+  QuerySnapshot searchSnapshot;
+  initiateSearch() {
+    dataBaseMethods
+        .getUserByUsername(searchTextEditingController.text)
+        .then((val) {
+      setState(() {
+        searchSnapshot = val;
+      });
+    });
+  }
+
+  Widget searchList() {
+    return searchSnapshot != null ? ListView.builder(
+      itemCount: searchSnapshot.documents.length,
+      shrinkWrap: true,
+      itemBuilder: (context, ind) {
+        return SearchTile(
+          userName: searchSnapshot.documents[ind].data["name"],
+          userEmail: searchSnapshot.documents[ind].data["email"],
+        );
+      },
+    ) : Container();
+  }
+  @override
+  void initState() {
+    initiateSearch();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,34 +50,65 @@ class _SearchScreenState extends State<SearchScreen> {
           children: [
             Container(
               color: Colors.blue[100],
-              padding: EdgeInsets.symmetric(horizontal: 24,vertical: 8),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
               child: Row(
                 children: [
-                  Expanded(child: TextField(
+                  Expanded(
+                      child: TextField(
                     controller: searchTextEditingController,
                     decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search Username.."
-
-                    ),
+                        border: InputBorder.none,
+                        hintText: "Search Username.."),
                   )),
                   GestureDetector(
-                    onTap: (){
-                      dataBaseMethods.getUserByUsername(searchTextEditingController.text).then((val){
-                        print(val.toString());
-                      });
+                    onTap: () {
+                      initiateSearch();
                     },
-                                      child: Container(
-                      height: 40,
-                      width: 40,
-                      padding: EdgeInsets.all(5),
-                      child: Image.asset("assets/images/search_white.png")),
+                    child: Container(
+                        height: 40,
+                        width: 40,
+                        padding: EdgeInsets.all(5),
+                        child: Image.asset("assets/images/search_white.png")),
                   ),
                 ],
               ),
             ),
+            searchList(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SearchTile extends StatelessWidget {
+  final String userName;
+  final String userEmail;
+  SearchTile({this.userName, this.userEmail});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(userName),
+              Text(userEmail),
+            ],
+          ),
+          Spacer(),
+          Container(
+            width: 100,
+            height: 35,
+            child: Center(child: Text("Chat",style: TextStyle(fontSize: 17),)),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(15)),
+          )
+        ],
       ),
     );
   }
